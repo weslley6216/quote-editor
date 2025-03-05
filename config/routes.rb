174404c5781d4e026f761724a638bb705a5fd3fc
Weslley Campos <weslley.campos@rebase.com.br>
2025-03-04 22:36:30 -0300

    Update locale and routes for line item

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :quotes do
-    resources :line_item_dates, except: [ :index, :show ]
+    resources :line_item_dates, except: %i[index show] do
+      resources :line_items, except: %i[index show]
+    end
   end
 end
